fee8a00caa2e565aacea86d812f5b247a29db0fb
Your Name <you@example.com>
2020-12-05 17:45:55 -0500

    Jheyson CRUD-cliente

--- ProyectoIntegrador/src/main/java/upeu/dao/DAOClienteImpl.java
+++ ProyectoIntegrador/src/main/java/upeu/dao/DAOClienteImpl.java
@@ -20,7 +20,19 @@ public class DAOClienteImpl extends Conexion implements DAOCliente {
             while (rs.next()){
                 Cliente clt = new Cliente();
                 clt.setIdc(rs.getInt(1));
+                clt.setCodC(rs.getString(2));
+                clt.setTipoC(rs.getString(3));
+                clt.setIdp_F(rs.getInt(4));
                 clt.setIdp(rs.getInt(5));
+                clt.setNombre(rs.getString(6));
+                clt.setAp_Paterno(rs.getString(7));
+                clt.setAp_Materno(rs.getString(8));
+                clt.setF_Nacimiento(rs.getString(9));
+                clt.setSexo(rs.getString(10));
+                clt.setDirection(rs.getString(11));
+                clt.setTelefono(rs.getInt(12));
+                clt.setCorreo(rs.getString(13));
+                clt.setDni(rs.getString(14));
                 lista.add(clt);
             }
         }catch (Exception e){
@@ -38,7 +50,19 @@ public class DAOClienteImpl extends Conexion implements DAOCliente {
             ResultSet rs = pstmt.executeQuery();
             if(rs.next()){
                 clt.setIdc(rs.getInt(1));
+                clt.setCodC(rs.getString(2));
+                clt.setTipoC(rs.getString(3));
+                clt.setIdp_F(rs.getInt(4));
                 clt.setIdp(rs.getInt(5));
+                clt.setNombre(rs.getString(6));
+                clt.setAp_Paterno(rs.getString(7));
+                clt.setAp_Materno(rs.getString(8));
+                clt.setF_Nacimiento(rs.getString(9));
+                clt.setSexo(rs.getString(10));
+                clt.setDirection(rs.getString(11));
+                clt.setTelefono(rs.getInt(12));
+                clt.setCorreo(rs.getString(13));
+                clt.setDni(rs.getString(14));
             }else {
 
             }
@@ -54,6 +78,8 @@ public class DAOClienteImpl extends Conexion implements DAOCliente {
         try {
             PreparedStatement pstmt = conectar().prepareStatement("INSERT INTO cliente(COD, TIPO, IDP_F) VALUES (?,?,?)");
             pstmt.setString(1,clt.getCodC());
+            pstmt.setString(2,clt.getTipoC());
+            pstmt.setInt(3,clt.getIdp_F());
             pstmt.executeUpdate();
         }catch (Exception e){
             JOptionPane.showMessageDialog(null,"Error: "+e.getMessage());
@@ -64,7 +90,14 @@ public class DAOClienteImpl extends Conexion implements DAOCliente {
     @Override
     public void actualizar(Cliente clt) {
         try {
-            PreparedStatement pstmt = conectar().prepareStatement("");
+            PreparedStatement pstmt = conectar().prepareStatement("UPDATE cliente SET"+
+                   " COD = ?, " +
+                   " TIPO = ?, " +
+                   " IDP_F = ? " +
+                   " WHERE IDC = ?");
+            pstmt.setString(1, clt.getCodC());
+            pstmt.setString(2, clt.getTipoC());
+            pstmt.setInt(3, clt.getIdp_F());
             pstmt.executeUpdate();
         }catch (Exception e){
             JOptionPane.showMessageDialog(null,"Error: "+e.getMessage());
@@ -75,7 +108,8 @@ public class DAOClienteImpl extends Conexion implements DAOCliente {
     @Override
     public void eliminar(Cliente clt) {
         try {
-            PreparedStatement pstmt = conectar().prepareStatement("DELETE FROM cliente c, persona p, WHERE c.IDP_F = p.IDP and IDP = ?");
+            PreparedStatement pstmt = conectar().prepareStatement("DELETE FROM cliente WHERE IDP = ?");
+            pstmt.setInt(1, clt.getIdc());
             pstmt.executeUpdate();
         }catch (Exception e){
             JOptionPane.showMessageDialog(null,"Error: "+e.getMessage());
